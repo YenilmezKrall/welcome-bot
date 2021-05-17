7f2adc2aaea113665dd6271303e4c6b8c0150543
YenilmezKrall <84318346+YenilmezKrall@users.noreply.github.com>
2021-05-17 17:44:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.Token = `tokeni yaz`; //Bot Token - Important
 exports.Default_Prefix = `s!`; //Bot Default Prefix - Important
-exports.Color = `blue`; //Bot All Embeds Color - Use CAPS For Name - Important
-exports.WelcomeImage = ``; //Welcome Image Link - Default Image By Me (If No Link Provided)
-exports.LeaveImage = ``; //Leave Image Link - Default Image By Me (If No Link Provided)
-exports.Support = ``; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
+exports.Color = `blue`; //Bot All Embeds Color - isim için kapaklar kullanın-önemli
+exports.WelcomeImage = ``; //Welcome Image Link - Varsayılan Resim Benim Tarafımdan (Bağlantı Sağlanmamışsa)
+exports.LeaveImage = ``; //(Eğer Bağlantı Sağlandı İse Bana Göre Görüntü Bağlantısı Varsayılan Görüntü Bırakın )
+exports.Support = ``; //Destek Sunucusu Bağlantısı - Asla PES Etmeyeceğim (Bağlantı Sağlanmazsa)
 //Make Sure To On Intents!
